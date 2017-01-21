75fbfa05b53959ba4f9551775e7d46083b7e6ed4
Thomas Bekaert <thomas@bekaert.me>
2017-01-21 17:09:48 +0100

    Style, route linking and component for transaction row

--- app/routes/transactions.js
+++ app/routes/transactions.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.$.getJSON('http://private-5d708-interviewfront.apiary-mock.com/transactions');
+  }
 });
